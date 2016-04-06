cb93039d92ef4d8276a4abe75d2a8a36f51aefa0
lixinduan(段立新) <lixinduan@tencent.com>
2016-04-06 21:54:55 +0800

    增加数据转换

--- http.js
+++ http.js
@@ -2,7 +2,7 @@
  * @Author: lixinduan
  * @Date:   2016-03-31 10:13:03
  * @Last Modified by:   lixinduan
- * @Last Modified time: 2016-04-06 14:25:08
+ * @Last Modified time: 2016-04-06 15:05:56
  */
 
 'use strict';
@@ -18,7 +18,7 @@ var port = 8888;
 var server = http.createServer(function(req, res) {
     // 设置返回头
     res.writeHeader(200,{
-        'Content-Type' : 'text/plain;charset=utf-8'  // 添加charset=utf-8
+        'Content-Type' : 'text/html;charset=utf-8'  // 添加charset=utf-8
     }) ;
     // 从url参数中取出匹配地址，也就是配置文件的key值
     var key = url.parse(req.url).pathname;
@@ -26,7 +26,7 @@ var server = http.createServer(function(req, res) {
     var callback = url.parse(req.url, true).query.callback;
     // 返回的mock数据
     var templateData = {}
-    if( key != '/favicon.ico') {
+    if( key && key != '/favicon.ico' && callback) {
         // 在配置文件中取出mock模板数据，并生成mock数据
         templateData = mock.mock(require(config[key]));
         // 返回mock数据
---
+++ mime.js
@@ -0,0 +1,27 @@
+/*
+* @Author: lixinduan
+* @Date:   2016-04-06 15:27:37
+* @Last Modified by:   lixinduan
+* @Last Modified time: 2016-04-06 15:28:04
+*/
+'use strict';
+exports.types = {
+    'css': 'text/css',
+    'gif': 'image/gif',
+    'html': 'text/html',
+    'ico': 'image/x-icon',
+    'jpeg': 'image/jpeg',
+    'jpg': 'image/jpeg',
+    'js': 'text/javascript',
+    'json': 'application/json',
+    'pdf': 'application/pdf',
+    'png': 'image/png',
+    'svg': 'image/svg+xml',
+    'swf': 'application/x-shockwave-flash',
+    'tiff': 'image/tiff',
+    'txt': 'text/plain',
+    'wav': 'audio/x-wav',
+    'wma': 'audio/x-ms-wma',
+    'wmv': 'video/x-ms-wmv',
+    'xml': 'text/xml'
+};

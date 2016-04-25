b7deada2ce8894fa84301f89674216a6973f1815
lixinduan(段立新) <lixinduan@tencent.com>
2016-04-25 13:32:34 +0800

    修改配置文件

--- config.js
+++ config.js
@@ -2,12 +2,22 @@
 * @Author: lixinduan
 * @Date:   2016-03-31 20:44:41
 * @Last Modified by:   lixinduan
-* @Last Modified time: 2016-04-11 16:37:56
+* @Last Modified time: 2016-04-25 13:22:18
 */
 
 'use strict';
 var config = {
-    '/html/sucess': './html/sucess',
+    '/vip/payStatus': '/vip/payStatus',
+    '/vip/auth': '/vip/auth',
+    '/html/matchStatV37': '/html/matchStatV37',
+    '/kbs/matchVideoAll': '/kbs/matchVideoAll',
+    '/html/myGuess1': '/html/myGuess1',
+    '/html/myGuess': '/html/myGuess',
+    '/html/rank2': '/html/rank2',
+    '/html/rank1': '/html/rank1',
+    '/html/aaaa': '/html/aaaa',
+    '/html/rank': '/html/rank',
+    '/html/sucess': '/html/sucess',
     
     'test': 'test'
 };
